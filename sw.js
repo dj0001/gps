a231e74cab96390304346e39613f14f2ade29f12
dj <dj0@gmx.de>
2017-01-28 08:54:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 console.log("todo")
-
+/*
 this.addEventListener('install', function(event) {  // self
   event.waitUntil(
     caches.open('v1').then(function(cache) {
@@ -23,7 +23,7 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
-
+*/
 /*
 self.addEventListener('fetch', function(event) {
   event.respondWith(
@@ -99,3 +99,67 @@ self.addEventListener('activate', function(event) {
   );
 });
 */
+
+'use strict';
+
+const version = 'v1.01';
+const staticCachePrefix = 'wave-pd1-static-';
+const staticCacheName = staticCachePrefix + version;
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            // cache all the static assets required for offline use.
+            return cache.addAll([
+                './',
+                'index.htm'
+            ]);
+        }).then(() => {
+            // activate the new service worker immediately, without waiting for next load.
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            // remove any old caches once the new service worker is activated.
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return cacheName.startsWith(staticCachePrefix) && cacheName !== staticCacheName;
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        }).then(() => {
+            // tell service worker to take control of any open pages.
+            self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+
+    let request = event.request;
+    let url = new URL(request.url);
+
+    // only deal with requests on the same domain.
+    if (url.origin !== location.origin) {
+        return;
+    }
+
+    // for non-GET requests, go to the network.
+    if (request.method !== 'GET') {
+        event.respondWith(fetch(request));
+        return;
+    }
+
+    // for everything else look to the cahce first,
+    // then fall back to the network.
+    event.respondWith(
+        caches.match(request).then(response => {
+            return response || fetch(request);
+        })
+    );
+});
